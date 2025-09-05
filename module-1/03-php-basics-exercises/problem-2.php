01dd2be9fd5912c03fd68d11d728013f4fadcccc
Eubha Watson <vwatson@nait.ca>
2025-09-05 11:41:09 -0600

    Adds complete Lesson 03 solutions.

--- module-1/03-php-basics-exercises/problem-2.php
+++ module-1/03-php-basics-exercises/problem-2.php
@@ -3,6 +3,24 @@
 $title = "Problem 2 - Pythagorean Theorem";
 include 'includes/header.php';
 
+$adjacent = 14;
+$opposite = 17;
+
+/**
+ * Remember that when PHP evaluates an arithmetic expression, the order of operations is applied.
+ * 
+ * BEDMAS - brackets, exponents, division/multiplication, addition/subtraction
+ * 
+ * PEMDAS - parenthesis, exponents, multiplcation/division, addition/subtration
+ */
+
+$hypotenuse = sqrt($adjacent ** 2 + $opposite ** 2);
+
+// The number that PHP gives us is very long and not terribly user-friendly. We can round the value to two decimal places before we echo it out to the user.
+$hypotenuse = round($hypotenuse, 2);
+
+echo "<p>The hypotenuse of a right triangle with an adjacent length of $adjacent and an opposite lenth of $opposite is $hypotenuse.</p>";
+
 ?>
 
 <a href="index.php" class="btn btn-outline-primary">Back to Table of Contents</a>
--- module-1/03-php-basics-exercises/problem-3.php
+++ module-1/03-php-basics-exercises/problem-3.php
@@ -3,7 +3,27 @@
 $title = "Problem 3 - Four-Digit Sums";
 include 'includes/header.php';
 
+/**
+ * We could solve this mathematically; however, PHP is a weak-typed language. This means that the data type of a variable can mutate throughout the flow of the program.
+ * 
+ * In PHP, a string is actually an array of characters. We're going to create a string and add each of its values together.
+ */
+
+$string = '1234';
+
+$total = $string[0] + $string[1] + $string[2] + $string[3];
+
+echo "<p>The sum of each individual value in $string is $total.</p>";
+
 ?>
+
+<form action="problem-3.php" method="POST">
+    <label for="num-string">Please enter a number:</label>
+    <input type="number" id="num-string" name="num-string">
+
+    <input type="submit" id="submit" name="submit" value="Submit">
+</form>
+
 
 <a href="index.php" class="btn btn-outline-primary">Back to Table of Contents</a>
 
