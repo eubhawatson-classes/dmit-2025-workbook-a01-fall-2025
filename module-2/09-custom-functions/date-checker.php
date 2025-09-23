daae821224503658b406efa7cf4dfa42177ff7e9
Valerie Watson <vwatson@nait.ca>
2025-09-23 15:43:35 -0600

    Breaktime code for Lesson 09.

---
+++ module-2/09-custom-functions/date-checker.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Date Checker</title>
+    
+    <!-- BS CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <section class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-md-6">
+                <h1 class="text-center fw-light">Date Checker</h1>
+                <p class="text-center lead text-muted"></p>
+
+                <hr class="my-5">
+
+                <hr class="my-5">
+
+                
+            </div>
+        </div>
+    </section>
+  </body>
+</html>
