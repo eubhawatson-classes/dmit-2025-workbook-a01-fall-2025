873557bd4ec5105a1727ab29e8b68c926c311561
Valerie Watson <vwatson@nait.ca>
2025-11-04 15:45:11 -0700

    Completes Lesson 20 (Edit in CRUD).

--- module-4/20-crud-edit/public/edit.php
+++ module-4/20-crud-edit/public/edit.php
@@ -5,7 +5,7 @@ $introduction = "To edit a city to our database, click 'Edit' beside the city yo
 include 'includes/header.php';
 
 // We need to initalise a bunch of variables because we're dealing with multiple sets of data. We'll start by checking to see if there's a primary key in the query string. 
-$city_id = $_GET['city_id'] ?? $_POST['city_id'] ?? '';
+$city_id = $_GET['city_id'] ?? $_POST['city-id'] ?? '';
 
 // NOTE: We should also sanitize everything here.
 $city_id = filter_var($city_id, FILTER_VALIDATE_INT);
@@ -21,16 +21,38 @@ $existing_capital = $city['is_capital'] ?? '0';
 $existing_trivia = $city['trivia'] ?? '';
 
 // Next, we'll define the variables for all of the values form the user (i.e. whatever they give us in the form).
-$user_city_name = $_POST['city_name'] ?? '';
+$user_city_name = $_POST['city-name'] ?? '';
 $user_province = $_POST['province'] ?? '';
 $user_population = $_POST['population'] ?? '';
-$user_capital = $_POST['is_capital'] ?? '0';
+$user_capital = $_POST['capital'] ?? '0';
 $user_trivia = $_POST['trivia'] ?? '';
 
 $message = "";
 $alert_class = "alert-danger";
 
-// TODO: process form submission.
+// If the user has submitted the form (hit 'save' after editing), we need to validate their input and try updating the record in the database.
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $validation_result = validate_city_input($user_city_name, $user_province, $user_population, $user_capital, $user_trivia, $provincial_abbr);
+
+    if ($validation_result['is_valid']) {
+
+        $validated_data = $validation_result['data'];
+
+        // If the data is good, we will try to update.
+        if (update_city($validated_data['city_name'], $validated_data['province'], $validated_data['population'], $validated_data['capital'], $validated_data['trivia'], $city_id)) {
+
+            $alert_class = "alert-success";
+            $message = $validated_data['city_name'] . " was updated successfully.";
+
+        } else {
+            $message = "There was an error updating the city.";
+        }
+
+    } else {
+        // If the data is not valid, we'll give the user errors.
+        $message = implode("</p><p>", $validation_result['errors']);
+    } 
+}
 
 // This is our validation message block.
 if ($message != "") : ?>
--- module-4/20-crud-edit/public/includes/form.php
+++ module-4/20-crud-edit/public/includes/form.php
@@ -1,63 +1,67 @@
 <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="border border-secondary-subtle rounded shadow-sm p-3">
 
-<h2 class="fs-4 fs-light mb-4">City Details</h2>
+    <h2 class="fs-4 fs-light mb-4">City Details</h2>
 
-<!-- City Name -->
- <div class="mb-4">
-    <label for="city-name" class="form-label">City Name</label>
-    <input type="text" id="city-name" name="city-name" class="form-control" value="<?= htmlspecialchars($_POST['city-name'] ?? ($city['city_name'] ?? '')); ?>">
-    <p class="form-text">What is the name of your city, town, or village?</p>
- </div>
+    <!-- City Name -->
+    <div class="mb-4">
+        <label for="city-name" class="form-label">City Name</label>
+        <input type="text" id="city-name" name="city-name" class="form-control" value="<?= htmlspecialchars($_POST['city-name'] ?? ($city['city_name'] ?? '')); ?>">
+        <p class="form-text">What is the name of your city, town, or village?</p>
+    </div>
 
-<!-- Province or Territory -->
- <div class="mb-4">
-    <label for="province" class="form-label">Province or Territory</label>
-    <select name="province" id="province" class="form-select">
-        <!-- We'll use a default option here; otherwise, the user might forget to choose a province and submit the first item in the array by mistake. -->
-        <option value="">-- Please Select --</option>
-        <?php
+    <!-- Province or Territory -->
+    <div class="mb-4">
+        <label for="province" class="form-label">Province or Territory</label>
+        <select name="province" id="province" class="form-select">
+            <!-- We'll use a default option here; otherwise, the user might forget to choose a province and submit the first item in the array by mistake. -->
+            <option value="">-- Please Select --</option>
+            <?php
             // Let's generate the rest of the options for the user. We'll also check to see if they previously selected a province. 
             foreach ($provincial_abbr as $key => $value) {
-                $selected = isset($_POST['province']) && $_POST['province'] == $key ? 'selected' : '';
+                $selected = ($_POST['province'] ?? $city['province'] ?? '') === $key ? 'selected' : '';
 
                 echo "<option value=\"$key\" $selected>$value</option>";
             }
+            ?>
+        </select>
+    </div>
+
+    <!-- Population -->
+    <div class="mb-4">
+        <label for="population" class="form-label">Population</label>
+        <input type="text" id="population" name="population" class="form-control" value="<?= htmlspecialchars($_POST['population'] ?? ($city['population'] ?? '')); ?>">
+        <p class="form-text">What is the approximate population of the city?</p>
+    </div>
+
+    <!-- Capital City -->
+    <fieldset class="mb-4">
+        <legend class="fw-normal fs-6">Is this city the capital of its province or territory?</legend>
+
+        <?php
+        $capital = $_POST['capital']
+            ?? (isset($city['is_capital']) ? ($city['is_capital'] ? '1' : '0') : '0');
         ?>
-    </select>
- </div>
 
-<!-- Population -->
- <div class="mb-4">
-    <label for="population" class="form-label">Population</label>
-    <input type="text" id="population" name="population" class="form-control" value="<?php if (isset($_POST['population'])) echo $_POST['population']; ?>">
-    <p class="form-text">What is the approximate population of the city?</p>
- </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="capital id="is-capital" value="1" <?= $capital === '1' ? 'checked' : '' ?>>
+            <label class="form-check-label" for="is-capital">Yes</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="capital" id="not-capital" value="0" <?= $capital === '0' ? 'checked' : '' ?>>
+            <label class="form-check-label" for="not-capital">No</label>
+        </div>
+    </fieldset>
 
-<!-- Capital City -->
- <fieldset class="mb-4">
-    <legend class="fw-normal fs-6">Is this city the capital of its province or territory?</legend>
-
-    <div class="form-check">
-        <input type="radio" id="is-capital" name="capital" value="1" class="form-check-input" <?= (isset($_POST['capital']) && $_POST['capital'] === '1') ? 'checked': ''; ?>>
-        <label for="is-capital" class="form-check-label">Yes</label>
+    <!-- Trivia -->
+    <div class="mb-4">
+        <label for="trivia" class="form-label">City Trivia (Optional)</label>
+        <input type="text" id="trivia" name="trivia" class="form-control" value="<?= htmlspecialchars($_POST['trivia'] ?? ($city['trivia'] ?? '')); ?>">
+        <p class="form-text">You may add a fun fact or piece of trivia for your city, in 255 characters or fewer.</p>
     </div>
 
-    <div class="form-check">
-        <input type="radio" id="not-capital" name="capital" value="0" class="form-check-input" <?= (isset($_POST['capital']) && $_POST['capital'] === '0') ? 'checked': ''; ?>>
-        <label for="not-capital" class="form-check-label">No</label>
-    </div>
- </fieldset>
+    <!-- City ID -->
+    <input type="hidden" name="city-id" id="city-id" value="<?= htmlspecialchars($_GET['city_id'] ?? $_POST['city-id'] ?? ''); ?>">
 
-<!-- Trivia -->
- <div class="mb-4">
-    <label for="trivia" class="form-label">City Trivia (Optional)</label>
-    <input type="text" id="trivia" name="trivia" class="form-control" value="<?php if (isset($_POST['trivia'])) echo $_POST['trivia']; ?>">
-    <p class="form-text">You may add a fun fact or piece of trivia for your city, in 255 characters or fewer.</p>
- </div>
-
-<!-- City ID -->
-<input type="hidden" name="city-id" id="city-id" value="<?= htmlspecialchars($_GET['city_id'] ?? $_POST['city_id'] ?? ''); ?>">
-
-<!-- Submit -->
- <input type="submit" id="submit" name="submit" value="Save" class="btn btn-lg btn-dark my-4">
+    <!-- Submit -->
+    <input type="submit" id="submit" name="submit" value="Save" class="btn btn-lg btn-dark my-4">
 </form>
